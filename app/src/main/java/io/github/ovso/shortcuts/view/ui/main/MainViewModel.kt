4c1c256277a3f39b9bff1f052d7f769f5aca0d8c
jaeho <ovsoce@gmail.com>
2019-07-10 23:28:21 +0900

    Logic divide

--- app/src/main/java/io/github/ovso/shortcuts/view/ui/main/MainViewModel.kt
+++ app/src/main/java/io/github/ovso/shortcuts/view/ui/main/MainViewModel.kt
@@ -13,22 +13,28 @@ class MainViewModel(private val args: MainViewModelArguments) : DisposableViewMo
   fun fetchList() {
     args.schdulers?.let {
       Observable.fromCallable {
-        val apps = args.resProvider?.toJsonStr("apps.json")
-        apps?.let {
-          val jsonArray = Gson()
-            .fromJson(apps, JsonElement::class.java)
-            .asJsonObject["apps"]
-            .asJsonArray
-          val array = Gson().fromJson(jsonArray, Array<App>::class.java)
-          val items = array.toMutableList()
-          items.sortWith(
-            Comparator { o1, o2 ->
-              o1!!.name.compareTo(o2!!.name, true)
-            }
-          )
-          appsLiveData.postValue(items)
-        }
-      }.subscribeOn(it.io())
+        // assets to json
+        args.resProvider?.toJsonStr("apps.json")
+      }.map {
+        // jsonStr to JsonArray
+        Gson()
+          .fromJson(it, JsonElement::class.java)
+          .asJsonObject["apps"]
+          .asJsonArray
+      }.map {
+        // jsonArray to items
+        val array = Gson().fromJson(it, Array<App>::class.java)
+        array.toMutableList()
+      }.map {
+        // items sort
+        it.sortWith(
+          Comparator { o1, o2 ->
+            o1!!.name.compareTo(o2!!.name, true)
+          }
+        )
+        appsLiveData.postValue(it)
+      }
+        .subscribeOn(args.schdulers.io())
         .autoDisposable(args.scopeProvider!!)
         .subscribe()
     }
